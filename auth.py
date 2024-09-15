c19a26e2dc4b102df744ed98296acb5becca6391
evan <Evan.Hugues@etu.univ-grenoble-alpes.fr>
2024-09-16 00:58:58 +0200

    menu mod

--- auth.py
+++ auth.py
@@ -1,5 +1,23 @@
 import configparser
 import os
+import sys
+
+def banner():
+    os.system('cls')
+    print(f'''
+ ____                  _      _       _
+| __ )  __ _ _   _  __| | ___| | __ _(_)_ __ ___
+|  _ \ / _` | | | |/ _` |/ _ \ |/ _` | | '__/ _ \,
+| |_) | (_| | |_| | (_| |  __/ | (_| | | | |  __/
+|____/ \__,_|\__,_|\__,_|\___|_|\__,_|_|_|  \___|
+                     _
+ _ __  _ __ _____  _(_) ___  ___
+| '_ \| '__/ _ \ \/ / |/ _ \/ __|
+| |_) | | | (_) >  <| |  __/\__ \¤
+| .__/|_|  \___/_/\_\_|\___||___/
+|_|
+          ''')
+    
 
 config = configparser.RawConfigParser()
 
@@ -11,6 +29,7 @@ config.read('proxies.ini')
 
 
 def add_proxy():
+    banner()
     proxy_count = len(config.sections()) + 1  # Compter les sections actuelles pour numéroter les proxies
     section_name = f'proxy{proxy_count}'
     
@@ -34,11 +53,15 @@ def add_proxy():
     # Écrire dans le fichier proxies.ini
     with open('proxies.ini', 'w') as configfile:
         config.write(configfile)
+    banner()
     print(f"[+] Proxy {section_name} added successfully!\n")
 
+
 def main():
-    print("[!] Proxy Setup")
+    banner()
+    print("[+] Proxy Setup")
     while True:
+        banner()
         add_proxy()
 
         another = input("[+] Add another proxy? (y/n): ").lower()
@@ -47,6 +70,7 @@ def main():
 
 
 def test_proxy(proxy):
+    banner()
     import requests
     proxies = {
         'http': f"socks5://{proxy['username']}:{proxy['password']}@{proxy['addr']}:{proxy['port']}",
@@ -63,19 +87,77 @@ def test_proxy(proxy):
 
 
 
+def display_proxies(proxies_file='proxies.ini'):
+    banner()
+    config = configparser.ConfigParser()
+    config.read(proxies_file)
 
+    if not config.sections():
+        print("[!] No proxies found!")
+        return
+    print("[+] Lists of proxies (q = quit):")
+    for i, section in enumerate(config.sections(), 1):
+        print(f"{i}. {section}\n")
+        
+    choix = input("[+] Enter a number to view proxy details : ")
+    if choix.lower() == 'q':
+        return
+    try:
+        chosen_section = config.sections()[int(choix) - 1]
+    except (ValueError, IndexError):
+        print("[!] Invalid choice!")
+        return
+    
+    print(f"\n[+] Proxy Details: {chosen_section}")
+    proxy_info = dict(config.items(chosen_section))
+    for cle, valeur in proxy_info.items():
+        print(f"{cle}: {valeur}")
+    
+    edit = input("\n[+] Edit this proxy? (y/n): ").lower()
+    if edit != 'y':
+        return
+
+    for cle in proxy_info.keys():
+        new_value = input(f"[+] Enter new value for {cle} (press enter to keep): ")
+        if new_value:
+            config.set(chosen_section, cle, new_value)
+
+    with open(proxies_file, 'w') as configfile:
+        config.write(configfile)
+
+    print(f"[+] Proxy from {chosen_section} updated successfully!")    
+
+def menu():
+    print("\n=== Menu ===")
+    print("[1]. Add Proxy")
+    print("[2]. Test Proxy")
+    print("[3]. Display & Edit Proxies")
+    print("[4]. Quit")
+    choix = input("\n[*] Select your option : ")
+    return choix
 
 
 if __name__ == '__main__':
-    #main()
-    proxy = {
-    'proxy_type': 'socks5',
-    'addr': 'residential.digiproxy.cc',
-    'port': 9595,
-    'username': 'u1WhTbxkIrSoAiY-res_sc-us_louisiana_Neworleans',
-    'password': 'dmucen0uiXuhPHd',
-    'rdns': True
-    }
-    test_proxy(proxy)
-    # Appel de la fonction asynchrone
-    
+    while True:
+        banner() 
+        choix = menu() 
+        
+        if choix == '1':
+            print("\nAdd Proxy...")
+            main() 
+
+        elif choix == '2':
+            proxy = input("Enter you proxy in that (format : socks5://user:pass@host:port) : ")
+            print(f"\n[!] Testing the Proxy : {proxy}...")
+            test_proxy(proxy)  
+
+        elif choix == '3':
+            print("\n[*] Here your proxies :")
+            display_proxies()  
+
+        elif choix == '4':
+            print("\n[*] Bye")
+            sys.exit(0) 
+
+        else:
+            print("\n[/!\] Invalid choice, please try again...")
