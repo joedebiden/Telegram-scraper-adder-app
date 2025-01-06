4c32e8ebe6822db4f6cd3b061a44d02ac3305bc1
evan <Evan.Hugues@etu.univ-grenoble-alpes.fr>
2025-01-06 02:07:42 +0100

    style : fixing style of app_settings

--- telebox/gui/app_settings.py
+++ telebox/gui/app_settings.py
@@ -7,7 +7,7 @@ from features.checker import RateLimiter
 class UserSettings(ctk.CTk):
     def __init__(self, user_email):
         super().__init__()
-        self.title("Settings")
+        self.title("Telebox - Settings")
         self.geometry("800x550")
         self.user_email = user_email
         
@@ -22,19 +22,43 @@ class UserSettings(ctk.CTk):
 
         # ====== Frame gauche (Commandes) ======
      
-        self.logo_label = ctk.CTkLabel(self.left_frame, text="Settings", font=ctk.CTkFont(size=24, weight="bold"))
+        self.logo_label = ctk.CTkLabel(self.left_frame, text="Settings", font=ctk.CTkFont("Helvetica Neue", size=24, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
-        self.info_button = ctk.CTkButton(self.left_frame, text="User Info", command=self.show_user_info)
+        self.info_button = ctk.CTkButton(
+            self.left_frame, 
+            text="User Info", 
+            command=self.show_user_info,
+            width=220, height=35, corner_radius=10, 
+            font=("Helvetica Neue", 16),
+            fg_color="#3b82f6", 
+            hover_color="#1e40af"
+            )
         self.info_button.grid(row=1, column=0, padx=20, pady=10)
 
         # self.license_button = ctk.CTkButton(self.left_frame, text="Check your subscriptions", command=self.show_license_page)
         # self.license_button.grid(row=2, column=0, padx=20, pady=10)
 
-        self.password_button = ctk.CTkButton(self.left_frame, text="Change Password", command=self.show_password_page)
+        self.password_button = ctk.CTkButton(
+            self.left_frame, 
+            text="Change Password", 
+            command=self.show_password_page,
+            width=220, height=35, corner_radius=10, 
+            font=("Helvetica Neue", 16),
+            fg_color="#3b82f6", 
+            hover_color="#1e40af"
+            )
         self.password_button.grid(row=3, column=0, padx=20, pady=10)
 
-        self.about_button = ctk.CTkButton(self.left_frame, text="Contact Support", command=self.open_website)
+        self.about_button = ctk.CTkButton(
+            self.left_frame, 
+            text="Contact Support", 
+            command=self.open_website,
+            width=220, height=35, corner_radius=10, 
+            font=("Helvetica Neue", 16),
+            fg_color="#3b82f6", 
+            hover_color="#1e40af"
+            )
         self.about_button.grid(row=4, column=0, padx=20, pady=10)
 
 
@@ -48,7 +72,7 @@ class UserSettings(ctk.CTk):
         self.right_frame.grid_columnconfigure(1, weight=0)
         self.right_frame.grid_columnconfigure(2, weight=1)
 
-        ctk.CTkLabel(self.right_frame, text="Your informations", font=ctk.CTkFont(size=20, weight="bold")).grid(row=0, column=1, pady=20)
+        ctk.CTkLabel(self.right_frame, text="Your informations", font=ctk.CTkFont("Helvetica Neue", size=20, weight="bold")).grid(row=0, column=1, pady=20)
 
         try: 
             response = requests.post(
@@ -68,16 +92,16 @@ class UserSettings(ctk.CTk):
                 self.create_copiable_entry("Transaction amount:", f"{transaction_amount} €", 3)
                 self.create_copiable_entry("Subscription plan:", subscription_plan, 4)
             else:
-                ctk.CTkLabel(self.right_frame, text="Failed to display your information").grid(row=5, column=1, pady=10)
+                ctk.CTkLabel(self.right_frame, text="Failed to display your information", font=("Helvetica Neue", 16),).grid(row=5, column=1, pady=10)
 
         except Exception as e:
-            ctk.CTkLabel(self.right_frame, text="Error with server connection...").grid(row=5, column=1, pady=10)
+            ctk.CTkLabel(self.right_frame, text="Error with server connection...", font=("Helvetica Neue", 16)).grid(row=5, column=1, pady=10)
 
 
 
     def create_copiable_entry(self, label_text, value, row):
         """Crée un label (titre) et un champ CTkEntry en readonly"""
-        ctk.CTkLabel(self.right_frame, text=label_text).grid(row=row, column=0, pady=10, sticky="e")
+        ctk.CTkLabel(self.right_frame, text=label_text, font=("Helvetica Neue", 16)).grid(row=row, column=0, pady=10, sticky="e")
         entry = ctk.CTkEntry(self.right_frame, width=250)
         entry.grid(row=row, column=1, pady=10, padx=10, sticky="w")
         
@@ -97,12 +121,12 @@ class UserSettings(ctk.CTk):
         ctk.CTkLabel(
             self.right_frame, 
             text="Change Your Password", 
-            font=ctk.CTkFont(size=20, weight="bold")
+            font=ctk.CTkFont("Helvetica Neue", size=20, weight="bold")
         ).grid(row=0, column=1, pady=20)
 
         # Champs pour valider l'ancien mot de passe
         ctk.CTkLabel(self.right_frame, text="Current Password:").grid(row=1, column=0, pady=10, sticky="e")
-        self.current_password_entry = ctk.CTkEntry(self.right_frame, show="*")
+        self.current_password_entry = ctk.CTkEntry(self.right_frame)
         self.current_password_entry.grid(row=1, column=1, pady=10, padx=10, sticky="w")
 
         # Champs pour entrer le nouveau mot de passe
@@ -118,7 +142,11 @@ class UserSettings(ctk.CTk):
         ctk.CTkButton(
             self.right_frame, 
             text="Update Password", 
-            command=self.change_password
+            command=self.change_password,
+            width=220, height=35, corner_radius=10, 
+            font=("Helvetica Neue", 16),
+            fg_color="#3b82f6", 
+            hover_color="#1e40af"
         ).grid(row=4, column=1, pady=20)
 
 
